7f5c73ed08e10f3fc9441209bb700be3f796fcdb
shapiro7 <57553889+shapiro7@users.noreply.github.com>
2020-07-29 19:28:57 +0900

    fps test
    숫자를 입력하면 그 수만큼 오브젝트가 생성되어 fps check

---
+++ HiddenObjects/Assets/SamplesResources/SceneAssets/GroundPlane/Scripts/Astronaut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Astronaut : MonoBehaviour
+
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int TextResult;
+    [SerializeField] private InputField inputField;
+
+
+
+
+
+    public void Input()
+    {
+        TextResult = int.Parse(inputField.text);
+
+        for (int i = 0; i < TextResult; i++)
+        {
+            if (i % 2 == 1)
+                Instantiate(prefab, new Vector3(transform.position.x + 0.05f * i, transform.position.y, transform.position.z), Quaternion.identity, GameObject.Find("Anchor_Plane").transform);
+            else
+                Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.05f * i), Quaternion.identity, GameObject.Find("Anchor_Plane").transform);
+
+        }
+    }
+
+
+}
